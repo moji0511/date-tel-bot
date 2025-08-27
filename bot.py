3394cf7ba48f682df0b34a20f0cd2a122800acab
moji0511 <moji.0511@gmail.com>
2025-08-27 20:59:09 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,45 @@
 import logging
-from telegram.ext import Application
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+from datetime import datetime
+import jdatetime
+import pytz
 
-TOKEN = "8345723232:AAGNNNYZdcHG2snUUaBaa2Ss_QS-X4h3oLk"
+# توکن ربات از Environment Variable
+TOKEN = os.getenv("BOT_TOKEN")
 
-logging.basicConfig(level=logging.INFO)
+if not TOKEN:
+    raise ValueError("❌ BOT_TOKEN در Environment Variables ست نشده است!")
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+async def date_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    tz = pytz.timezone("Asia/Tehran")
+    now = datetime.now(tz)
+
+    # تاریخ میلادی
+    gregorian = now.strftime("%A %d %B %Y")
+
+    # تاریخ شمسی
+    jalali = jdatetime.datetime.fromgregorian(datetime=now).strftime("%A %d %B %Y")
+
+    msg = (
+        f"📅 تاریخ امروز:\n\n"
+        f"🌞 شمسی: {jalali}\n"
+        f"🌍 میلادی: {gregorian}\n\n"
+        f"مخصوص گروه بچه‌های ایرون @iran9897"
+    )
+
+    await update.message.reply_text(msg)
 
 def main():
     print("🚀 Bot is starting...")
     app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("date", date_command))
     app.run_polling()
 
 if __name__ == "__main__":
