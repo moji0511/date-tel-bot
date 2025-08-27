0fbcfab48d958883b642822aeb430b8aa9e817d5
moji0511 <moji.0511@gmail.com>
2025-08-27 16:17:14 +0330

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from datetime import datetime
-from persiantools.jdatetime import JalaliDate
+from khayyam import JalaliDatetime
 
 TOKEN = "8345723232:AAGNNNYZdcHG2snUUaBaa2Ss_QS-X4h3oLk"
 
@@ -12,7 +11,7 @@ async def date_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     gregorian_str = gregorian.strftime("%A, %B %d, %Y")
 
     # تاریخ شمسی
-    jalali = JalaliDate.today()
+    jalali = JalaliDatetime.now()
     jalali_str = jalali.strftime("%A %d %B %Y")
 
     message = f"📅 تاریخ امروز:\n{jalali_str}\n{gregorian_str}\n\nمخصوص گروه بچه‌های ایرون @iran9897"
