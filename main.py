514f40f456b1947a7b74cc0cc80aed092b750552
moji0511 <moji.0511@gmail.com>
2025-08-27 16:08:54 +0330

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import logging
+
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from datetime import datetime
-import jdatetime
+from persiantools.jdatetime import JalaliDate
 
 TOKEN = "8345723232:AAGNNNYZdcHG2snUUaBaa2Ss_QS-X4h3oLk"
-
-logging.basicConfig(level=logging.INFO)
 
 async def date_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # تاریخ میلادی
@@ -14,13 +12,13 @@ async def date_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     gregorian_str = gregorian.strftime("%A, %B %d, %Y")
 
     # تاریخ شمسی
-    jalali = jdatetime.datetime.now()
+    jalali = JalaliDate.today()
     jalali_str = jalali.strftime("%A %d %B %Y")
 
     message = f"📅 تاریخ امروز:\n{jalali_str}\n{gregorian_str}\n\nمخصوص گروه بچه‌های ایرون @iran9897"
     await update.message.reply_text(message)
 
-if name == "main":
+if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("date", date_command))
     app.run_polling()
